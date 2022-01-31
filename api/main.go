891b7c1b162be57a2445ba67fc25d1db31c78520
Deepayan <deepayanmukherjee1312@gmail.com>
2022-01-31 20:34:00 +0530

    Added main function and env variables

--- api/main.go
+++ api/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
+)
+
 func main() {
+
+	err := godotenv.Load()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	app := fiber.New()
+
+	app.Use(logger.New())
+
+	app.Get(":/url", routes.resolveURL)
+	app.Post(":/api/v1", routes.shortenURL)
+
+	log.Fatal(app.Listen(":5000"))
 
 }
