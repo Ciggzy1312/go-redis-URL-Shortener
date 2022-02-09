a9d2259cb2b40a710b531b2f79a0e56d090ed540
Deepayan <deepayanmukherjee1312@gmail.com>
2022-02-09 21:44:54 +0530

    Added remove domain function

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,6 +1,12 @@
 package routes
 
-import "time"
+import (
+	"time"
+
+	"github.com/Ciggzy1312/url-shortener/helpers"
+
+	"github.com/gofiber/fiber"
+)
 
 type request struct {
 	URL         string        `json:"url"`
@@ -14,4 +20,19 @@ type response struct {
 	Expiry          time.Duration `json:"expiry"`
 	XRateRemaining  int           `json:"rate_limit"`
 	XRateLimitReset time.Duration `json:"rate_limit_reset"`
+}
+
+func shortenURL(c *fiber.Ctx) error {
+	body := new(request)
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"err": "cannot parse JSON"})
+	}
+
+	// check domain error
+
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"err": "Domain error"})
+	}
+
 }
