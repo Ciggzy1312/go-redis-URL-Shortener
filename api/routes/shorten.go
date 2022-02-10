6a540f367ae6711f7aa0b161fdba006ac415a060
Deepayan <deepayanmukherjee1312@gmail.com>
2022-02-10 20:17:01 +0530

    Added rate limiting function

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,9 +1,13 @@
 package routes
 
 import (
+	"os"
+	"strconv"
 	"time"
 
+	"github.com/Ciggzy1312/url-shortener/database"
 	"github.com/Ciggzy1312/url-shortener/helpers"
+	"github.com/go-redis/redis/v8"
 
 	"github.com/gofiber/fiber"
 )
@@ -34,5 +38,29 @@ func shortenURL(c *fiber.Ctx) error {
 	if !helpers.RemoveDomainError(body.URL) {
 		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"err": "Domain error"})
 	}
+
+	//implement rate limits
+
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+
+	val, err := r2.Get(database.Ctx, c.IP()).Result()
+
+	if err == redis.Nil {
+		_ = r2.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+	} else {
+		val, _ = r2.Get(database.Ctx, c.IP()).Result()
+		valInt, _ := strconv.Atoi(val)
+
+		if valInt <= 0 {
+			limit, _ := r2.TTL(database.Ctx, c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+				"err":             "rate limit exceeded",
+				"rate limit left": limit / time.Nanosecond / time.Minute,
+			})
+		}
+	}
+
+	r2.Decr(database.Ctx, c.IP())
 
 }
